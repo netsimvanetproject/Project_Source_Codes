055c83b840347309357aa4bcfc0719c154a8c91b
anonymous <netsimvanetproject@gmail.com>
2018-12-10 21:40:39 +0530

    updated

--- LTE/LTE_enum.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-#include "LTE_enum.h"
-#undef _NETSIM_LTE_ENUM_H_
-#define GENERATE_ENUM_STRINGS
-#include "LTE_enum.h"
-#undef GENERATE_ENUM_STRINGS
-
--- Token_BR/Token_enum.c
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-#include "Token_enum.h"
-#define GENERATE_ENUM_STRINGS
-#include "Token_enum.h"
-#undef GENERATE_ENUM_STRINGS
